a681678a41f8baa6a18dbcc95b31c4fb221c2f30
odidoddixb <158168278+odidoddixb@users.noreply.github.com>
2026-05-09 20:00:18 +0900

    1,2단계 주석추가

--- snake.cpp
+++ snake.cpp
@@ -3,16 +3,16 @@
 
 //뱀
 std::deque<Point> snake; //뱀의 좌표 저장
-Direction dir = RIGHT;
-bool gameOver = false;
+Direction dir = RIGHT; // 현재 이동 방향 (오른쪽으로)
+bool gameOver = false; // 게임오버 false로 초기화
 
 //뱀을 맵 중간쯤에 머리 1칸, 몸통 2칸으로 초기화
 void initSnake(){
     snake.push_back({10, 3}); //머리
     snake.push_back({10, 2}); //몸통
     snake.push_back({10, 1}); //몸통
-    map[10][3]=3;
-    map[10][2]=4;
+    map[10][3]=3; // 3이 머리
+    map[10][2]=4; // 4가 몸통
     map[10][1]=4;
 }
 
@@ -21,12 +21,13 @@ void moveSnake(){
     Point head = snake.front();
     Point newHead = head;
 
+    // 방향따라 새로운 머리 위치 계산
     if(dir == UP) newHead.y--;
     if(dir == DOWN) newHead.y++;
     if(dir == LEFT) newHead.x--;
     if(dir == RIGHT) newHead.x++;
 
-    //새 머리 좌표 계산
+    //새 머리 좌표 확인
     int cell = map[newHead.y][newHead.x];
     if (cell==1 || cell==2 || cell==4){ //머리위치가 벽, 부술 수 없는 벽, 자기 몸이면 게임오버
         gameOver=true;
