63ee61896fe458c3ea86c2844dc69b2dd0211aa6
odidoddixb <158168278+odidoddixb@users.noreply.github.com>
2026-05-09 20:08:31 +0900

    1,2 단계 주석 추가

--- snake.hpp
+++ snake.hpp
@@ -1,16 +1,16 @@
-#ifndef SNAKE_HPP
+#ifndef SNAKE_HPP // snake_hpp 아직 정의 안 됐으면 아래 코드 사용
 #define SNAKE_HPP
 
 #include <deque>
 
-enum Direction {UP, DOWN, LEFT, RIGHT};
-struct Point {int y,x;};
+enum Direction {UP, DOWN, LEFT, RIGHT}; // 뱀 이동 방향 저장
+struct Point {int y,x;}; // 좌표 저장
 
-extern std::deque<Point> snake;
-extern Direction dir;
-extern bool gameOver;
+extern std::deque<Point> snake; // 뱀 좌표 저장
+extern Direction dir; // 현재 이동 방향 저장
+extern bool gameOver; // 게임 종료 확인
 
-void initSnake();
-void moveSnake();
+void initSnake(); // 뱀 초기화 함수
+void moveSnake(); // 뱀 이동 함수
 
-#endif
+#endif // SNAKE_HPP 끝
