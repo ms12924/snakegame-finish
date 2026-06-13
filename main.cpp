52419c870354b9dd02e453d0bd15d0c4ad472c51
ms12924 <ja334009@gmail.com>
2026-06-13 19:09:32 +0900

    최종본 완성

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#define MILESTONE 5  // 1=맵, 2=뱀이동, 3=아이템, 4=게이트, 5=스코어+미션
-
 #include <ncurses.h>
 #include <cstdlib>
 #include <ctime>
@@ -14,23 +12,13 @@ int main() {
     srand(static_cast<unsigned>(time(nullptr)));
 
     Board    board;
-#if MILESTONE >= 2
     Snake    snake;
-#endif
-#if MILESTONE >= 3
     Food     food;
     Poison   poison;
-#endif
-#if MILESTONE >= 4
     Gate     gate;
-#endif
-#if MILESTONE >= 5
     Contents contents;
     bool     youwin = false;
-#endif
-#if MILESTONE >= 4
-    bool specialGateEntered = false;
-#endif
+    bool     specialGateEntered = false;
 
     int currentStage = 0;
 
@@ -60,33 +48,17 @@ int main() {
     init_pair(8, COLOR_BLUE,    COLOR_BLUE);
     init_pair(9, COLOR_CYAN,    COLOR_CYAN);
 
-#if MILESTONE >= 2
     snake.init(board, currentStage);
-#endif
-#if MILESTONE >= 3
     food.init(board, currentStage);
-#endif
-#if MILESTONE >= 4
     gate.init(board, currentStage);
-#endif
-#if MILESTONE >= 5
     contents.loadHighScore();
     contents.init(snake);
-#endif
 
-#if MILESTONE >= 2
     int tick = 0;
-    while (!snake.isGameOver()) {
-#else
-    while (true) {
-#endif
+    while (!snake.isGameOver())
+    {
         int key = getch();
 
-#if MILESTONE == 1
-        if (key == 'q') break;
-#endif
-
-#if MILESTONE >= 2
         Direction dir = snake.getDir();
         if (key == KEY_UP    && dir != DOWN)  snake.setDir(UP);
         if (key == KEY_DOWN  && dir != UP)    snake.setDir(DOWN);
@@ -107,12 +79,9 @@ int main() {
 
         tick++;
         snake.tickInvincible();
-#endif
-#if MILESTONE >= 5
-        if (tick % 10 == 0) contents.tickTime();
-#endif
 
-#if MILESTONE >= 2
+        if (tick % 10 == 0) contents.tickTime();
+
         if (tick % 2 == 0) {
             Point     head    = snake.getHead();
             Direction curDir  = snake.getDir();
@@ -123,52 +92,35 @@ int main() {
             else if (curDir == LEFT)  newHead.x--;
             else if (curDir == RIGHT) newHead.x++;
 
-#if MILESTONE >= 3
             int headType = board.getCell(currentStage, newHead.y, newHead.x);
-#endif
 
             snake.move(board, currentStage);
 
-#if MILESTONE >= 3
             if (headType == 5) {
                 food.applyGrowth(snake, board, currentStage);
-#if MILESTONE >= 5
                 contents.addGrowth();
-#endif
             }
             if (headType == 8) {
                 food.applySpecial(snake);
             }
             if (headType == 6) {
                 poison.apply(snake, board, currentStage);
-#if MILESTONE >= 5
                 contents.addPoison();
-#endif
             }
-#endif
-#if MILESTONE >= 4
             if (headType == 7) {
                 gate.teleport(snake, board, currentStage);
-#if MILESTONE >= 5
                 contents.addGate();
-#endif
             }
             if (headType == 9) {
                 gate.deactivateSpecial();
                 specialGateEntered = true;
             }
-#endif
         }
-#endif
-#if MILESTONE >= 4
         gate.update(board, currentStage);
         gate.updateSpecial(board, currentStage, snake, currentStage == Board::STAGE_COUNT - 1);
-#endif
 
-#if MILESTONE >= 3
         food.update(board, currentStage);
-#endif
-#if MILESTONE >= 5
+
         contents.updateScore(snake);
 
         if (contents.checkMission(currentStage) || specialGateEntered) {
@@ -187,34 +139,21 @@ int main() {
             gate.init(board, currentStage);
             contents.init(snake);
         }
-#endif
 
-#if MILESTONE >= 2
         board.draw(currentStage, snake.isInvincible());
-#else
-        board.draw(currentStage, false);
-#endif
-#if MILESTONE >= 5
         contents.draw(currentStage, snake.getHearts());
-#endif
         refresh();
         napms(100);
     }
 
-#if MILESTONE >= 2
-#if MILESTONE >= 5
     if (!youwin)
-#endif
     {
         mvprintw(Board::MAP_SIZE / 2, Board::MAP_SIZE - 4, "GAME OVER");
         refresh();
         napms(2000);
     }
-#endif
 
-#if MILESTONE >= 5
     contents.saveHighScore(currentStage);
-#endif
 
     endwin();
     return 0;
